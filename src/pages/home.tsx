338f6e36e3c1d4b1b396901becb8faf4dce2cdc8
Nihal Thappi <nihal.t567@gmail.com>
2023-08-10 13:17:36 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,18 +1,30 @@
 import WeatherReport from "../components/WeatherReport";
 import WeatherForm from "../components/WeatherForm";
 import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 function Home() {
     const [current, setcurrent] = useState<any>()
 
-    const userdata = (weatherdata: string) => {
-        console.log(weatherdata)
-        fetch(`https://api.weatherapi.com/v1/forecast.json?key=46fd613d614b46799e253109230208&q=${weatherdata}&days=7&aqi=no&alerts=no`)
-            .then(response => response.json())
-            .then((data) => {
-                setcurrent(data);
-            })
-            .catch((error) => console.log("Error", error));
-    }
+    const userdata = async (weatherdata: string) => {
+        console.log(weatherdata);
+        
+        try {
+            const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=46fd613d614b46799e253109230208&q=${weatherdata}&days=7&aqi=no&alerts=no`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            else{
+                const data = await response.json();
+            setcurrent(data);
+            }
+            
+        } catch (error ) {
+           toast.error("Enter a valid Location");
+        }
+    };
     // useEffect(() => {
     //     console.log("current state:", current); // Log the current state whenever it changes
     // }, [current]);
@@ -24,6 +36,7 @@ function Home() {
             ) : (
                 <p></p>
             )}
+            <ToastContainer position="top-center"/>
         </div>
 
     )
